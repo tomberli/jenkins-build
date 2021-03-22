b5b579e92e1ab76ff0cca579483f3ba1e3e8dc35
limingxing <451721318@qq.com>
2021-03-22 17:28:52 +0800

    Initial commit

---
+++ src/main/java/com/jenkins/test/JenkinsBuildApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBuildApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jenkins/test/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.jenkins.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author mingxing.li02@hand-china.com
+ * @Date 2021/3/22 17:11
+ */
+@RestController
+@RequestMapping("/jenkins")
+public class TestController {
+
+    /**
+     * localhost:8099/jenkins/test
+     * @return
+     */
+    @GetMapping("/test")
+    public String test() {
+        return "jenkins部署111";
+    }
+
+
+
+}
