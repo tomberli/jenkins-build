bc1606eba3c2e081a485d56b904c81fe65af2c30
limingxing <451721318@qq.com>
2021-03-22 17:44:21 +0800

    fix:测试

--- src/main/java/com/jenkins/test/controller/TestController.java
+++ src/main/java/com/jenkins/test/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.jenkins.test.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
